88742a77972caf482e4c5c0b8e20f547edfa1ce9
hey880 <hey880@naver.com>
2022-11-03 00:41:33 +0900

    Modify: clean up Post Controller

--- TestProject/src/main/java/studio/thinkground/testproject/controller/PostController.java
+++ TestProject/src/main/java/studio/thinkground/testproject/controller/PostController.java
@@ -4,19 +4,35 @@ import java.util.Map;
 
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import dto.MemberDTO;
 
 @RestController
+@RequestMapping("/api/v1/post-api")
 public class PostController {
-    @PostMapping(value="/postAnything")
-    public String val (@RequestBody Map<String, Object> map) {
-        return "srt";
+
+    @PostMapping(value="/default")
+    public String postMethod () {
+        return "Hello World";
     }
 
-    @PostMapping(value="/postDto")
+    // http://localhost:8080/api/v1/post-api/member
+    @PostMapping(value="/member")
+    public String postMember (@RequestBody Map<String, Object> postData) {
+        StringBuilder sb = new StringBuilder();
+
+        postData.entrySet().forEach(map -> {
+            sb.append(map.getKey() + " : " + map.getValue() + "\n");
+        });
+
+        return sb.toString();
+    }
+
+    // http://localhost:8080/api/v1/post-api/member2
+    @PostMapping(value="/member2")
     public String member (@RequestBody MemberDTO memberDTO) {
-        return "srt";
+        return memberDTO.toString();
     }
 }
